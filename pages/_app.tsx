f35cfd05bb6874d0e3227a4f3b3b51e65bb18a08
NatanTavares <nattantavares.s15@gmail.com>
2021-05-13 21:10:20 -0300

    feat(styles): Implements the global styling of the application using StyledComponents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import { GlobalStyle } from "../styles/globals";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
       <GlobalStyle />
+      <Component {...pageProps} />
     </>
   );
 }
