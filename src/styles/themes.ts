66e72099a017f23d22654162b954f3c6d29164c1
NatanTavares <nattantavares.s15@gmail.com>
2021-05-21 22:37:25 -0300

    feat(styles): Create a theme

---
+++ src/styles/themes.ts
@@ -0,0 +1,35 @@
+export const discord = {
+  colors: {
+    backgroundDark: "#2F3136",
+    background: "#36393F",
+    backgroundLight: "#72767D",
+
+    backgroundButtonGray: "#474C53",
+
+    red: "#ED4245",
+    green: "#3BA55C",
+    blue: "#7289DA",
+    white: "#FFFFFF",
+
+    textTitle: "#8E9297",
+    textBody: "#DCDDDE",
+  },
+};
+
+export const github = {
+  colors: {
+    backgroundDark: "#0D1117",
+    background: "#0D1117",
+    backgroundButtonGray: "#30363D",
+
+    red: "#DA3633",
+    green: "#2ea043",
+
+    blue: "#79C0F2",
+    white: "#FFFFFF",
+
+    textTitle: "#c9d1d9",
+    textBody: "#DCDDDE",
+  },
+};
+
