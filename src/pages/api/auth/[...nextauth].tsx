6dfea531774b6299411d6c584f25be2ad12926fe
NatanTavares <nattantavares.s15@gmail.com>
2021-05-21 23:47:39 -0300

    feat(auth): Implement Github provider

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -6,6 +6,12 @@ export default NextAuth({
     Providers.Discord({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      scope: "rpc guilds identify",
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: "user",
     }),
   ],
 });
